0cf7f9ff52f6da6c231a095913f44d528ffc0ccd
gordan <ifyousmile0410@gmail.com>
2021-05-18 17:59:48 +0800

    Feature: 設定static與新增Bootstrap環境
    1. 於pms_index內建立static, media資料夾並設定static路徑(包括BASE_DIR, STATICFILES_DIR,  STATIC_ROOT, MEDIAURL, MEDIA_ROOT)
    2. 於static資料夾內新增Bootstrap檔案(包括bootstrap.min.css, bootstrap.min.js, jquery-3.5.1.slim.min.js, popper.min.js) 以及GitLab_Log.png
    3. .gitignore修改正確media路徑
    
    issue #2

--- Project_Manage_System/Project_Manage_System/settings.py
+++ Project_Manage_System/Project_Manage_System/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'Project_Manage_System.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
@@ -117,8 +118,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "/pms_index", "/static"),
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR + '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
