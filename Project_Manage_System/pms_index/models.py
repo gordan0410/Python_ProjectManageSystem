95b61df80dcb0fbf2b7158d1de5be6523bd9bf10
gordan <ifyousmile0410@gmail.com>
2021-05-19 19:16:24 +0800

    Feature: 建立Projects model
    1. 建立Projects model
    2. migrate為0001_initial.py
    3. 因為mirgrate 所以從test server 重新download下來整個project避免不相容
    
    issue #4

--- Project_Manage_System/pms_index/models.py
+++ Project_Manage_System/pms_index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Projects(models.Model):
+    name = models.CharField(max_length=50)
+    deadline = models.DateField(max_length=10)
+    descriptions = models.TextField(max_length=1000)
+    owner = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
