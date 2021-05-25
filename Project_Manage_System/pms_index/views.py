af2930b129a48b8794696e14a0577b2f1c73139f
gordan <ifyousmile0410@gmail.com>
2021-05-25 15:16:57 +0800

    Feature: 新增NewProject form及Projects model於views.index
    1. 新增NewProject form並設定驗證後儲存
    2. 新增Projects model以利查詢資料庫欄位資料
    
    issue #4

--- Project_Manage_System/pms_index/views.py
+++ Project_Manage_System/pms_index/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import NewProject
+from .models import Projects
 
 
 def index(request):
+
+    all_projects = Projects.objects.all()
+
+    new_project_form = NewProject()
+
+    if request.method == "POST":
+        new_project_form = NewProject(request.POST)
+        if new_project_form.is_valid():
+            new_project_form.save()
+        return redirect("/")
+
     return render(request, "index.html", locals())
 
 
