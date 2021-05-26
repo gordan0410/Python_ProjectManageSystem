6ac7419a69e0f8c443d6137902b935fafcaf96a7
gordan <ifyousmile0410@gmail.com>
2021-05-26 22:21:05 +0800

    Feature: 新增NewProjectMembers Form 並刪除NewProject的owner欄位
    1. 於form.py新增NewProjectMembers:
    member_id 以多選方式設置及顯示
    2. 刪除NewProject的owner欄位為不顯示, 預設為專案創立者
    
    issue #4

--- Project_Manage_System/pms_index/forms.py
+++ Project_Manage_System/pms_index/forms.py
@@ -1,21 +1,36 @@
 from django import forms
-from .models import Projects
+from .models import Projects, ProjectMembers
+from django.contrib.auth.models import User
 
 
 class NewProject(forms.ModelForm):
 
     class Meta:
         model = Projects
-        fields = ('name', 'deadline', 'descriptions', 'owner')
+        fields = ('name', 'deadline', 'descriptions')
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'deadline': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'descriptions': forms.Textarea(attrs={'class': 'form-control'}),
-            'owner': forms.TextInput(attrs={'class': 'form-control'}),
-            }
+        }
         labels = {
             'name': '專案名稱',
             'deadline': '專案截止日',
             'descriptions': '專案內容',
-            'owner': '專案擁有者'
+        }
+
+
+class NewProjectMembers(forms.ModelForm):
+
+    member_id: forms.ModelMultipleChoiceField(queryset=User.objects.none())
+
+    class Meta:
+
+        model = ProjectMembers
+        fields = ('member_id',)
+        widgets = {
+            'member_id': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'member_id': '專案使用者',
         }
