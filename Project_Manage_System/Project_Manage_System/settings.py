46c8888dffc4f74a2c5b2f9a7bfa77f5c9dbad0c
gordan <ifyousmile0410@gmail.com>
2021-05-26 22:15:05 +0800

    Feature: 新增model:project_members
    1.migrate 返回zero 本來要刪除owner但後來決定留下
    2. 新增ProjectMembers model 並 migrate: 0001_initial.py
    
    Table name: ProjectMembers
    columns:
    1. id
    2. project_id (primarykey from Project)
    3. member_id(manytomany from auth_user)
    4. member_status(owner, developer, manager)
    5. created_at
    
    issue #4

--- Project_Manage_System/Project_Manage_System/settings.py
+++ Project_Manage_System/Project_Manage_System/settings.py
@@ -131,6 +131,7 @@ MEDIA_ROOT = BASE_DIR + '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 try:
     from .local_settings import *
 except ImportError as e:
--- Project_Manage_System/pms_index/models.py
+++ Project_Manage_System/pms_index/models.py
@@ -10,3 +10,10 @@ class Projects(models.Model):
     descriptions = models.TextField(max_length=1000)
     owner = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now=True)
+
+
+class ProjectMembers(models.Model):
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    member_id = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    member_status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
