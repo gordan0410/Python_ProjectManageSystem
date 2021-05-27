ed274965186c1f7a69549e2440e14e4f695005c0
gordan <ifyousmile0410@gmail.com>
2021-05-27 13:56:22 +0800

    Feature: 新增NewProjectMembers於fuc: index
    1. 新增form: NewProjectMembers
    2. 新增預設owner為建立專案者
    3. 新增預設使用者為Developer
    4. 新增.save_m2m for ManytoManyfield
    
    issue #4

--- Project_Manage_System/pms_index/views.py
+++ Project_Manage_System/pms_index/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import NewProject
-from .models import Projects
+from .forms import NewProject, NewProjectMembers
+from .models import Projects, ProjectMembers
 
 
 def index(request):
@@ -9,12 +9,21 @@ def index(request):
     all_projects = Projects.objects.all()
 
     new_project_form = NewProject()
+    new_project_member_form = NewProjectMembers()
 
     if request.method == "POST":
         new_project_form = NewProject(request.POST)
-        if new_project_form.is_valid():
+        new_project_member_form = NewProjectMembers(request.POST)
+        if new_project_member_form.is_valid() and new_project_form.is_valid():
+            npfform = new_project_form.save(commit=False)
+            npfform.owner = request.user
             new_project_form.save()
-        return redirect("/")
+            npmform = new_project_member_form.save(commit=False)
+            npmform.project_id = npfform
+            npmform.member_status = "Developer"
+            npmform.save()
+            new_project_member_form.save_m2m()
+            return redirect("/")
 
     return render(request, "index.html", locals())
 
