5c000458030758056908998953b6a8436e297daf
gordan <ifyousmile0410@gmail.com>
2021-06-02 17:41:23 +0800

    Fix: 修飾view邏輯
    目的: 解決先前project永遠對應status: developer問題
    1. 以User.objects.exclude(username=request.user) 讓專案創立者與指派使用者分離
    2. User.objects.filter(username=request.user).first() 提取專案創立者
    3.  20-23行, 已選使用者+專案創立者, 使用在28行, 以分辨誰是owner及developer
    4.  npfform.status = "active" project_status預設為active
    
    issue #4

--- Project_Manage_System/pms_index/views.py
+++ Project_Manage_System/pms_index/views.py
@@ -1,28 +1,40 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import NewProject, NewProjectMembers
+from .forms import ProjectForm, ProjectMembersForm
 from .models import Projects, ProjectMembers
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.db.models import Q
 
 
-def index(request):
-
+def pms_index(request):
+    all_users = User.objects.exclude(username=request.user)
     all_projects = Projects.objects.all()
 
-    new_project_form = NewProject()
-    new_project_member_form = NewProjectMembers()
+    new_project_form = ProjectForm()
+    new_project_member_form = ProjectMembersForm()
 
     if request.method == "POST":
-        new_project_form = NewProject(request.POST)
-        new_project_member_form = NewProjectMembers(request.POST)
-        if new_project_member_form.is_valid() and new_project_form.is_valid():
+        new_project_form = ProjectForm(request.POST)
+        current_user = User.objects.filter(username=request.user).first()
+        data = dict(request.POST)
+        select_user = list(data['select_user'])
+        select_user = [int(x) for x in select_user]
+        select_user.append(current_user.id)
+        if new_project_form.is_valid():
             npfform = new_project_form.save(commit=False)
-            npfform.owner = request.user
-            new_project_form.save()
-            npmform = new_project_member_form.save(commit=False)
-            npmform.project_id = npfform
-            npmform.member_status = "Developer"
-            npmform.save()
-            new_project_member_form.save_m2m()
+            npfform.status = "active"
+            npfform.save()
+            for member_id in select_user:
+                member_model = ProjectMembers()
+                member_model.project_id = npfform
+                if member_id == current_user.id:
+                    member_model.member_status = "Owner"
+                else:
+                    member_model.member_status = "Developer"
+                member = User.objects.filter(id=member_id).first()
+                member_model.member_id = member
+                member_model.save()
             return redirect("/")
 
     return render(request, "index.html", locals())
