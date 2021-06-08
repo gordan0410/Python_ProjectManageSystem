e6151ddc5f93346c8c93b04366ff7a4a49c2d4e0
gordan <ifyousmile0410@gmail.com>
2021-06-08 11:58:04 +0800

    Feature: 新增RegisterForm and LoginForm
    1. 新增RegisterForm for 註冊
    2. 新增LoginForm for 登入
    
    issue #5

--- Project_Manage_System/pms_index/forms.py
+++ Project_Manage_System/pms_index/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from .models import Projects, ProjectMembers
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class NewProject(forms.ModelForm):
+class ProjectForm(forms.ModelForm):
 
     class Meta:
         model = Projects
@@ -20,17 +21,68 @@ class NewProject(forms.ModelForm):
         }
 
 
-class NewProjectMembers(forms.ModelForm):
+# class ProjectMembersForm(forms.ModelForm):
+#
+#     member_id: forms.ModelMultipleChoiceField(queryset=User.objects.none())
+#
+#     class Meta:
+#
+#         # model = ProjectMembers
+#         fields = ('member_id',)
+#         widgets = {
+#             'member_id': forms.SelectMultiple(attrs={'class': 'form-control'}),
+#         }
+#         labels = {
+#             'member_id': '專案使用者',
+#         }
 
-    member_id: forms.ModelMultipleChoiceField(queryset=User.objects.none())
+
+class RegisterForm(UserCreationForm):
+
+    last_name = forms.CharField(
+        label="姓",
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+
+    first_name = forms.CharField(
+        label="名字",
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+
+    email = forms.EmailField(
+        label="電子郵件",
+        widget=forms.EmailInput(attrs={'class': 'form-control'}),
+    )
+
+    username = forms.CharField(
+        label="帳號",
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+
+    password1 = forms.CharField(
+        label="密碼",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
+
+    password2 = forms.CharField(
+        label="確認密碼",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
 
     class Meta:
 
-        model = ProjectMembers
-        fields = ('member_id',)
-        widgets = {
-            'member_id': forms.SelectMultiple(attrs={'class': 'form-control'}),
-        }
-        labels = {
-            'member_id': '專案使用者',
-        }
+        model = User
+        fields = {'last_name', 'first_name', 'email', 'username', 'password1', 'password2'}
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(
+        label="帳號",
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    password = forms.CharField(
+        label="密碼",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
+
